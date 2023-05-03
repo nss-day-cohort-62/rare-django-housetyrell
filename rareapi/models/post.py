45255653e8cff680ca97c4aaf98fa735b7460960
Vision Filler <visionfiller@gmail.com>
2023-05-03 15:34:54 -0500

    fixtures

---
+++ rareapi/models/post.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+
+class Post(models.Model):
+
+    user= models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name='posts')
+    category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.SET_NULL, related_name='posts' )
+    title = models.CharField(max_length=155)
+    publication_date = models.DateField()
+    image_url = models.CharField(max_length=155)
+    content = models.CharField(max_length=155)
+    approved= models.BooleanField()
