c3aafee787f78639469ecaf34453a3e64def6a71
Vision Filler <visionfiller@gmail.com>
2023-05-04 15:15:23 -0500

    halfway done

--- rareapi/models/rareuser.py
+++ rareapi/models/rareuser.py
@@ -7,3 +7,6 @@ class RareUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=50)
 
+    @property
+    def full_name(self):
+        return f'{self.user.first_name} {self.user.last_name}'
