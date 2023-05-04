81f26d31caa6adae892387ff3f1f1e00870781e3
Mary Williams <sbyardnss@gmail.com>
2023-05-04 09:15:45 -0500

    toss commit

--- rareapi/models/comment.py
+++ rareapi/models/comment.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Comment(models.Model):
 
-    post_id = models.ForeignKey("Post", on_delete=models.CASCADE, related_name='comments')
-    author_id = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name='comments')
     content = models.CharField(max_length=155)
     created_on = models.DateField()
